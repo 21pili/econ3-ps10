f98b18690b2419d128653d0eb0a0a25286831443
21pili <90769845+21pili@users.noreply.github.com>
2024-04-30 17:50:33 +0200

    first commit i guess

--- code.R
+++ code.R
@@ -2,28 +2,43 @@
 ### Libraries
 library(mlogit)
 library(tidyverse)
+library(stargazer)
 
 
 ### Imports
 data("Fishing", package = "mlogit")
 
 # divide income by 1000
-Fishing$income <- Fishing$income/1000
+fish <- Fishing %>% mutate(income =  income / 1000)
 
-# change the order of the variables - will be helpful later on
-Fishing <- Fishing[c(1,10,seq(4)+5,seq(4)+1)]
+### (a)
 
-# drop "beach"
-Fishing <- Fishing[which(Fishing$mode!="beach"),]             # drops observations that chose beach
-to_drop <- names(Fishing) %in% c("price.beach","catch.beach")
-Fishing <- Fishing[!to_drop]                                  # drops the variables in to_drop
-Fishing <- droplevels(Fishing)                                # "cleans" the data
+# turning the data.frame into a dfidx object
+fish <- dfidx(fish, varying = 2:9, shape = "wide", choice = "mode")
+# fitting the conditional logit
+conditional_logit <- mlogit(mode ~ 0 + price + catch, data = fish, reflevel = "boat") #nolint
+summary(conditional_logit)
 
-# (a) - replace ...
-CL <- mlogit(..., data = Fishing, shape = "wide", reflevel = "boat")
-summary(CL)
+### (c)
 
-# (b)
-# numerators of probabilities
-Fishing$numpier <- exp(cbind(Fishing$price.pier,Fishing$catch.pier) %*% CL$coefficients)
-# ...
+subset_logit <- mlogit(mode ~ 0 + price + catch, data = fish, alt.subset = c("charter", "pier")) #nolint
+summary(subset_logit)
+
+hausman_test <- hmftest(conditional_logit, subset_logit)
+print(hausman_test)
+
+### (d)
+
+multinomial_logit <- mlogit(mode ~ 0 | income, data = fish)
+summary(multinomial_logit)
+
+### (e)
+
+mixed_logit <- mlogit(mode ~ 0 + price + catch | income, data = fish)
+summary(mixed_logit)
+
+
+stargazer(conditional_logit, multinomial_logit, mixed_logit, type = "latex",
+                    out = "OUTPUT/regressions.tex",
+                    label = "reg",
+                    title = "Coefficients of the Representative Utility")
